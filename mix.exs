8728eaecb97da2fcd527b776b38e671b4aa5cc91
Rhett Del Fierro <thinquetank@gmail.com>
2017-04-17 16:54:17 -0700

    added Guardian and configs for it.

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Bullsource.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
+    [{:phoenix, "~> 1.3.0-rc", override: true},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
@@ -36,7 +36,8 @@ defmodule Bullsource.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 3.0"}]
+     {:comeonin, "~> 3.0"},
+     {:guardian, "~> 0.14"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
