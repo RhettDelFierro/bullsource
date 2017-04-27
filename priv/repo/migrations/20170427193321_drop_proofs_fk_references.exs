143eac6140c327390a51c84c6c4eed1837ab9f47
Rhett Del Fierro <thinquetank@gmail.com>
2017-04-27 12:37:51 -0700

    executed new migration.

--- priv/repo/migrations/20170427193321_drop_proofs_fk_references.exs
+++ priv/repo/migrations/20170427193321_drop_proofs_fk_references.exs
@@ -2,6 +2,16 @@ defmodule Bullsource.Repo.Migrations.DropProofsFkReferences do
   use Ecto.Migration
 
   def change do
+    drop_if_exists index(:references, [:proof_id])
+    create table(:proof_references) do
+
+      add :proof_id, references(:proofs, on_delete: :delete_all)
+      add :reference_id, references(:references, on_delete: :delete_all)
+
+      timestamps()
+    end
+
+    create unique_index(:proof_references, [:proof_id, :reference_id])
 
   end
 end
