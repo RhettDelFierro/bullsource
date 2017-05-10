d931ed53fb2ab9e9eaf8fcf08a96486ce5c57755
Rhett Del Fierro <thinquetank@gmail.com>
2017-05-10 14:10:15 -0700

    the multiple create/2 actions are matching clauses, planning out next step.

--- lib/bullsource/web/controllers/api/vote_controller.ex
+++ lib/bullsource/web/controllers/api/vote_controller.ex
@@ -2,30 +2,43 @@ defmodule Bullsource.Web.VoteController do
   use Bullsource.Web, :controller
 
   alias Bullsource.Votes
-  alias BullSource.Votes.{PostVoteDown, PostVoteUp, ProofVoteDown,
-                          ProofVoteUp, ReferenceVoteDown, ReferenceVoteDown}
   alias Bullsource.Helpers.Converters
 
 
-  def create(conn,{"post_vote" = post_vote}) do
+  def create(conn,%{"post_vote" => post_vote}) do
     user = Guardian.Plug.current_resource(conn)
     params = Converters.str_to_atom_keys(post_vote)
-    IO.inspect params
+    case params.vote_type do
+      "down" ->
+        Votes.down_vote_post(params)
+      "up"   ->
+        Votes.up_vote_post(params)
+    end
 
   end
 
-  def create(conn,{"proof_vote" = proof_vote}) do
+  def create(conn,%{"proof_vote" => proof_vote}) do
     user = Guardian.Plug.current_resource(conn)
     params = Converters.str_to_atom_keys(proof_vote)
     IO.inspect params
 
   end
 
-  def create(conn,{"reference_vote" = reference_vote}) do
+  def create(conn,%{"reference_vote" => reference_vote}) do
     user = Guardian.Plug.current_resource(conn)
     params = Converters.str_to_atom_keys(reference_vote)
     IO.inspect params
 
   end
+#  this one will get called if the user already voted. this is where we'll repo.get, delete it then create a new vote in the other table.
+  def update(conn, params) do
 
+  end
+
+  defp render_vote(tuple,conn) do
+    case tuple do
+      {:ok, changeset} ->
+      {:ok, error_changeset} ->
+    end
+  end
 end
