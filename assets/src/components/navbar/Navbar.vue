dd7b1c2a21f038b581f0b48c44f255eff0ddfc69
Rhett Del Fierro <thinquetank@gmail.com>
2017-05-18 13:03:14 -0700

    made some component contexts about to try and register.

---
+++ assets/src/components/navbar/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="register"></button>
+  </div>
+</template>
+<script>
+  export default {
+      methods: {
+          register() {
+
+          }
+      }
+  }
+</script>
+<style>
+
+</style>
--- assets/src/store/modules/user.js
+++ assets/src/store/modules/user.js
@@ -34,7 +34,6 @@ const actions = {
           reject();
         }
       })
-
     })
   },
   [types.LOGIN]: ({commit}, payload) => {
