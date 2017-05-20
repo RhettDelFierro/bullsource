fb17c49c2f898a467da8ae45e08d49a00693afcf
Rhett Del Fierro <thinquetank@gmail.com>
2017-05-20 07:24:59 -0700

    graphql schema and router

---
+++ lib/bullsource/graphql/schema.ex
@@ -0,0 +1,7 @@
+defmodule Bullsource.GraphQL.Schema do
+#defines the shape of our api. We can practically do away with the router.
+#this schema is a plug
+  use Absinthe.Schema
+
+
+end
--- lib/bullsource/web/router.ex
+++ lib/bullsource/web/router.ex
@@ -1,47 +1,5 @@
 defmodule Bullsource.Web.Router do
   use Bullsource.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
-    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
-    plug Guardian.Plug.LoadResource
-  end
-
-  pipeline :require_login do
-    plug Guardian.Plug.EnsureAuthenticated, handler: Bullsource.Web.AuthController
-  end
-
-  # Other scopes may use custom stacks.
-   scope "/api", Bullsource.Web do
-     pipe_through :api
-     post "/sessions", SessionController, :create #logon
-     #delete "/sessions", SessionController, :delete
-
-     resources "/topics", TopicController, only: [:index]
-     resources "/threads", ThreadController, only: [:index]
-     resources "/posts", PostController, only: [:index]
-
-     resources "/users", UserController #for registering and soon to be updating.
-
-     #another scope for the protected routes?
-   end
-
-   #mainly for routes that do everything except only read it.
-   scope "/api", Bullsource.Web do
-     pipe_through [:api, :require_login]
-
-     resources "/topics", TopicController, except: [:index]
-     resources "/threads", ThreadController, except: [:index]
-     resources "/posts", PostController, except: [:index]
-     resources "/votes", VoteController, only: [:create, :update, :delete]
-   end
-
+  forward "/graphql", Absinthe.Plug.GraphQL, schema: Bullsource.GraphQL.Schema
 end
