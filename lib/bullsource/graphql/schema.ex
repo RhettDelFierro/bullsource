96637c8158a08ed19d3df114ccc764029c6b701b
Rhett Del Fierro <thinquetank@gmail.com>
2017-05-20 08:04:18 -0700

    added :threads field to be resolved in :topic object. graphql schema working so far.

--- lib/bullsource/graphql/schema.ex
+++ lib/bullsource/graphql/schema.ex
@@ -2,25 +2,33 @@ defmodule Bullsource.GraphQL.Schema do
 #defines the shape of our api. We can practically do away with the router.
 #this schema is a plug
   use Absinthe.Schema
+#  alias Bullsource.Discussion.{Article, Comment, Post, Proof, Reference, Thread, Topic}
+#  alias Bullsource.Accounts.User
+#  alias Bullsource.Votes.{PostVoteUp, PostVoteDown, ProofVoteUp,
+#                                ProofVoteDown, ReferenceVoteUp, ReferenceVoteDown}
+  alias Bullsource.Repo
   alias Bullsource.Discussion.{Article, Comment, Post, Proof, Reference, Thread, Topic}
   alias Bullsource.Accounts.User
   alias Bullsource.Votes.{PostVoteUp, PostVoteDown, ProofVoteUp,
                                 ProofVoteDown, ReferenceVoteUp, ReferenceVoteDown}
   @desc "Topics has many threads."
   object :topic do
-    field :name, :string, unique: true
+    field :id, :integer
+    field :name, :string
     field :description, :string
 
+    #because :topic has_many :threads, we're going to add a :threads field
+    field :threads, list_of(:thread) do
+      resolve(_args, %{source: topic}) -> #we can define how every field is resolved with the resolve/2 function.
+        topic = Repo.preload(topic, :threads)
+        {:ok, topic.threads}
+    end
   end
 
   @desc "Threads belong to toics and users. Has many posts."
   object :thread do
+    field :id, :integer
     field :title, :string
-
-    belongs_to :user, User
-    belongs_to :topic, Topic
-
-    has_many :posts, Post
 
     timestamps()
   end
@@ -28,7 +36,8 @@ defmodule Bullsource.GraphQL.Schema do
   query do
     field :topic, list_of(:topic) do
       resolve fn(_args, _context) ->
-      {:ok, Repo.all(Topic)}
+        {:ok, Repo.all(Topic)}
+      end
     end
   end
 end
