db97935980fc5809bab2ea58f4f9b0cdae2d9663
Rhett Del Fierro <thinquetank@gmail.com>
2017-05-20 12:47:07 -0700

    now making an efficient batch query for our threads by topic id.

--- lib/bullsource/graphql/resolvers/thread_resolver.ex
+++ lib/bullsource/graphql/resolvers/thread_resolver.ex
@@ -1,10 +1,16 @@
 defmodule Bullsource.GraqphQL.ThreadResolver do
-  alias Bullsource.Repo
-  alias Bullsource.Discussion.Thread
+  import Absinthe.Resolution.Helpers #batch query so it won't make a query for each topic to get a thread.
+  import Ecto.Query
+
+  alias Bullsource.{Repo, Discussion.Thread}
 
   def assoc(_args, %{source: topic}) do
-    topic = Repo.preload(topic, :threads)
-    {:ok, topic.threads}
+    #now we're only making one query for all the threads int he topic:
+    batch {__MODULE__, :by_topic_id}, topic.id, fn results ->
+        {:ok, Map.get(results, topic.id)}
+    end
+#    topic = Repo.preload(topic, :threads)
+#    {:ok, topic.threads}
   end
 
   def create(%{title: title, topic_id: topic_id}, _context) do
@@ -12,4 +18,10 @@ defmodule Bullsource.GraqphQL.ThreadResolver do
     {:ok, thread}
   end
 
+  def by_topic_id(_, ids) do
+    Thread
+    |> where([t], t.topic_id in ^ids)
+    |> Repo.all
+    |> Enum.group_by(&(&1.topic_id))
+  end
 end
