87a46196f877816054d01da0aaeea34558ae1111
Rhett Del Fierro <thinquetank@gmail.com>
2017-05-20 07:40:30 -0700

    query for :topic

--- lib/bullsource/graphql/schema.ex
+++ lib/bullsource/graphql/schema.ex
@@ -2,6 +2,33 @@ defmodule Bullsource.GraphQL.Schema do
 #defines the shape of our api. We can practically do away with the router.
 #this schema is a plug
   use Absinthe.Schema
+  alias Bullsource.Discussion.{Article, Comment, Post, Proof, Reference, Thread, Topic}
+  alias Bullsource.Accounts.User
+  alias Bullsource.Votes.{PostVoteUp, PostVoteDown, ProofVoteUp,
+                                ProofVoteDown, ReferenceVoteUp, ReferenceVoteDown}
+  @desc "Topics has many threads."
+  object :topic do
+    field :name, :string, unique: true
+    field :description, :string
 
+  end
 
+  @desc "Threads belong to toics and users. Has many posts."
+  object :thread do
+    field :title, :string
+
+    belongs_to :user, User
+    belongs_to :topic, Topic
+
+    has_many :posts, Post
+
+    timestamps()
+  end
+
+  query do
+    field :topic, list_of(:topic) do
+      resolve fn(_args, _context) ->
+      {:ok, Repo.all(Topic)}
+    end
+  end
 end
--- lib/bullsource/web/router.ex
+++ lib/bullsource/web/router.ex
@@ -1,5 +1,5 @@
 defmodule Bullsource.Web.Router do
   use Bullsource.Web, :router
 
-  forward "/graphql", Absinthe.Plug.GraphQL, schema: Bullsource.GraphQL.Schema
+  forward "/graphql", Absinthe.Plug.GraphiQL, schema: Bullsource.GraphQL.Schema
 end
