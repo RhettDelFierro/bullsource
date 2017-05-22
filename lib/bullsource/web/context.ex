26e75daa5a5d53c2901a5e38c40803a281644913
Rhett Del Fierro <thinquetank@gmail.com>
2017-05-22 09:26:52 -0700

    Context Plug created for authentication.

--- lib/bullsource/web/context.ex
+++ lib/bullsource/web/context.ex
@@ -8,7 +8,7 @@ defmodule Bullsource.Web.Context do
   def call(conn, _ ) do
     case build_context(conn) do
       {ok, context} ->
-        put_private*conn, :absinthe, %{context: context}
+        put_private(conn, :absinthe, %{context: context})
       {:error, reason} ->
         conn
       _ ->
@@ -23,5 +23,17 @@ defmodule Bullsource.Web.Context do
     end
   end
 
+  defp authorize(token) do
+    case Guardian.decode_and_verify(token) do
+      {:ok, claims}    -> return_user(claims)
+      {:error, reason} -> {error, reason}
+    end
+  end
 
+  defp return_user(claims) do
+    case Guardian.serializer.from_token(Map.get(claims, "sub")) do
+      {:ok, resource}  -> {:ok, resource}
+      {:error, reason} -> {:error, reason}
+    end
+  end
 end
