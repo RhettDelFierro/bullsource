67aaf286c3b51ad6672fab425e22ed41648342bc
Rhett Del Fierro <thinquetank@gmail.com>
2017-06-02 23:18:25 -0700

    erros compiling poison, will try override

--- lib/bullsource/reference_validator/google_custom_search.ex
+++ lib/bullsource/reference_validator/google_custom_search.ex
@@ -1,10 +1,41 @@
 defmodule Bullsource.ReferenceValidator.GoogleCustomSearch do
+  @google_url "https://www.googleapis.com/customsearch/v1?"
 
   def start_link(url, url_ref, owner, limit) do
-    Task.start_link(__MODULE__, :get_info, [url, url_ref, owner, ])
+    Task.start_link(__MODULE__, :get_info, [url, url_ref, owner, limit])
   end
 
-  defp get_info(url) do
-    {:ok, url}
+  defp get_info(url, url_ref, owner, limit) do
+    url
+    |> build_query
+    |> make_request
+    |> send_results(query_ref, owner)
   end
+
+  defp build_query(url) do
+    api_key = build_api_key()
+    query = @google_url <> "key=#{api_key}" <> "&q=#{url}"
+    query
+  end
+
+  defp build_api_key do
+    Application.get_env(:bullsource, :google_custom_search)[:app_id]
+  end
+
+  defp make_request(query) do
+    case HTTPoison.get(query) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+         {:ok, body}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+         {:error, "404 error"}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+         {:error, reason}
+    end
+  end
+
+  defp send_results(answer, query_ref, owner) do
+    results = [%Result{result: to_string(answer)}]
+    send(owner, {:results, query_ref, results})
+  end
+
 end
--- lib/bullsource/reference_validator/reference_validator.ex
+++ lib/bullsource/reference_validator/reference_validator.ex
@@ -2,7 +2,7 @@ defmodule Bullsource.ReferenceValidator do
   @backends [Bullsource.ReferenceValidator.GoogleCustomSearch]
   
   defmodule Result do
-    defstruct url: nil, title: nil, valid: false
+    defstruct url: nil, title: nil, valid: false, result: nil
   end
 
   def verify_reference(url, opts \\ []) do
@@ -12,7 +12,7 @@ defmodule Bullsource.ReferenceValidator do
     |> Enum.map(&spawn_query(&1, url, limit))
   end
 
-  def start_link(url, backend, limit) do
+  def start_link(backend, url, url_ref, owner, limit) do
     backend.start_link(url)
   end
 
@@ -20,9 +20,9 @@ defmodule Bullsource.ReferenceValidator do
   defp spawn_query(backend, url, limit) do
     url_ref = make_ref() #unique identifier (e.g #Reference<0.0.2390148>)
     opts = [backend, url, url_ref, self(), limit] #self() will get the message to be sent into the verify_reference/2 pipeline. - this is called in Enum.map
-    {:ok, pid} = Supervisor.start_child(Bullsource.ReferenceValidator.Supervisor, opts) #spawn a a child
+    {:ok, pid} = Supervisor.start_child(Bullsource.ReferenceValidator.Supervisor, opts) #spawn a a child - context is this process we're in right now that the supervisor spawned.
     monitor_ref = Process.monitor(pid)
-    {pid,  monitor_ref, query_ref}
+    {pid,  monitor_ref, url_ref}
   end
 
   
