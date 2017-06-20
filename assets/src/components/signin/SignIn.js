93a3afb4df2e973a35b0e9eff186ba46ce2b9371
Rhett Del Fierro <thinquetank@gmail.com>
2017-06-20 12:30:46 -0700

    error handling after signin failure handled in react.

--- assets/src/components/signin/SignIn.js
+++ assets/src/components/signin/SignIn.js
@@ -1,8 +1,7 @@
 import React, {Component} from "react";
-import { graphql } from "react-apollo";
+import {graphql} from "react-apollo";
 import {Link, withRouter} from "react-router-dom";
-import newsTweetQuery from "../../queries/fetchNewsTweets";
-import signInMutation from "../../mutations/signin"
+import signInMutation from "../../mutations/signin";
 
 class SignIn extends Component {
     constructor(props) {
@@ -11,43 +10,60 @@ class SignIn extends Component {
         this.state = {
             username: '',
             password: '',
-            signInError: ''
+            validationMessage: '',
+            validationError: ''
         };
     }
 
     onSubmit(event) {
         event.preventDefault();
-
+        if (this.state.username === '') {
+            this.setState({validationError: 'username cannot be blank'});
+            return
+        }
+        if (this.state.password === '') {
+            this.setState({validationError: 'password cannot be blank'});
+            return
+        }
         //attempt mutation:
         this.props.mutate({
             variables: {
                 username: this.state.username,
                 password: this.state.password
             },
-            refetchQueries: [{query: newsTweetQuery}]
+            // refetchQueries: [{query: currentUser}]
         }).then((response) => {
-            let token = response.data.loginUser.token;
+            const {user, token} = response.data.loginUser;
+
             localStorage.setItem('token', token);
             this.setState({
                 username: '',
                 password: '',
-                signInError: ''
+                validationMessage: `Welcome back ${user.username}!`,
+                validationError: ''
             });
+
             this.props.history.push("/");
         })
             .catch((e) => {
+                if (e.graphQLErrors[0].message === "In field \"loginUser\": No user with this username was found!" ||
+                    e.graphQLErrors[0].message === "In field \"loginUser\": Incorrect password") {
+                    this.setState({usernameError: "No user with this username was found!"})
+                }
                 this.setState({
-                    signInError: 'Invalid Username and/or password.'
+                    validationError: 'Invalid Username and/or password.'
                 })
             })
     }
 
     render() {
-
+        let message = this.state.validationMessage || "Sign In!";
         return (
             <div>
                 <Link to="/">Back</Link>
-                <h3>Sign In!</h3>
+                <h3>{message}</h3>
+                <p style={{color: "red"}}>{this.state.validationError}</p>
+
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Username</label>
                     <input type="text"
@@ -60,7 +76,7 @@ class SignIn extends Component {
                            onChange={event => this.setState({password: event.target.value})}
                            value={this.state.password}
                     />
-                    <p>{this.state.signInError}</p>
+
 
                     <input type="submit" onClick={this.onSubmit.bind(this)}/>
                 </form>
@@ -69,7 +85,6 @@ class SignIn extends Component {
 
     }
 }
-
 
 
 export default graphql(signInMutation)(withRouter(SignIn));
