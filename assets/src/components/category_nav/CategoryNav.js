2735e20fef2304458fd3afc4d0b46031f6b4aebc
Rhett Del Fierro <thinquetank@gmail.com>
2017-06-21 13:52:20 -0700

    centered the category-nav

--- assets/src/components/category_nav/CategoryNav.js
+++ assets/src/components/category_nav/CategoryNav.js
@@ -1,38 +1,20 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom'
-import './style.css'
+import React from "react";
+import {NavLink} from "react-router-dom";
+import "./style.css";
 
 export const CategoryNav = () => {
     return (
-        <ul className='category-nav'>
-            <li>
-                <NavLink exact activeClassName='active' to='/'>Home</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/category/entertainment'>Entertainment</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/category/gaming'>Gaming</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/category/general'>General</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/category/music'>Music</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/category/politics'>Politics</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/category/science-and-nature'>Science and Nature</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/category/sport'>Sport</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/category/technology'>Technology</NavLink>
-            </li>
-        </ul>
+        <div className='category-nav'>
+            <NavLink exact activeClassName='active' to='/'>Home</NavLink>
+            <NavLink activeClassName='active' to='/category/entertainment'>Entertainment</NavLink>
+            <NavLink activeClassName='active' to='/category/gaming'>Gaming</NavLink>
+            <NavLink activeClassName='active' to='/category/general'>General</NavLink>
+            <NavLink activeClassName='active' to='/category/music'>Music</NavLink>
+            <NavLink activeClassName='active' to='/category/politics'>Politics</NavLink>
+            <NavLink activeClassName='active' to='/category/science-and-nature'>Science and Nature</NavLink>
+            <NavLink activeClassName='active' to='/category/sport'>Sport</NavLink>
+            <NavLink activeClassName='active' to='/category/technology'>Technology</NavLink>
+        </div>
     )
 };
 
