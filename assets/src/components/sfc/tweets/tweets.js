aedc91ac4db9aca0893f9b3a382a994ed37fee7b
Rhett Del Fierro <thinquetank@gmail.com>
2017-06-22 01:35:32 -0700

    margin on tweet rendering correctly.

--- assets/src/components/sfc/tweets/tweets.js
+++ assets/src/components/sfc/tweets/tweets.js
@@ -1,18 +1,32 @@
 import React from "react";
 import styles from "./style.css";
-import {Tweet} from 'react-twitter-widgets';
+import {Tweet} from "react-twitter-widgets";
 
 export const Tweets = ({toggle, showTweets, tweets}) => {
     return (
         showTweets ?
             <div className={styles['tweet-container']}>
-                {tweets.map(tweet => <Tweet tweetId={tweet.id_str} options={{cards: "hidden"}}/>)}
-                <i className="fa fa-caret-right" aria-hidden="true" onClick={toggle}/>
+                <div>
+                    {tweets.map(tweet => {
+                        return (
+                            <div className={styles['returned-tweet']}><Tweet key={tweet.id_str}
+                                        tweetId={tweet.id_str}
+                                        options={{
+                                            cards: "hidden",
+                                            hide_thread: "true",
+                                            conversation: "none"
+                                        }}/>
+                            </div>
+                        )
+                    })}
+                </div>
+                <i className="fa fa-caret-right" aria-hidden="true" onClick={toggle}
+                   style={{"gridArea": "1 / 1 / 2 / 3"}}/>
             </div>
             :
             <div className={styles['hidden-tweet-container']} onClick={toggle}>
                 <i className="fa fa-caret-left" aria-hidden="true"/>
-
+                {tweets.length}
                 <i className="fa fa-twitter" aria-hidden="true" style={{color: "#4AB3F4"}}/>
             </div>
     )
