b6f091cc806724e0024050a0e8a49ec775c62407
Rhett Del Fierro <thinquetank@gmail.com>
2017-06-24 02:04:02 -0700

    dropdown now working.

--- assets/src/components/header/dropdown/DropDown.js
+++ assets/src/components/header/dropdown/DropDown.js
@@ -16,23 +16,27 @@ class DropDown extends Component {
 
     render() {
         const dropDownClass = `${styles.overlay} ${styles["dropdown-content"]}`;
+        const dropDownIcon = this.state.opened ?
+            <i className="fa fa-caret-up" aria-hidden="true"/>
+            : <i className="fa fa-caret-down" aria-hidden="true"/>;
         return (
             <div className={styles['dropdown-container']} onClick={this.toggleDropdown}>
-                <div className={styles.dropdown} onClick={this.toggleDropdown.bind(this)}><i>Bullsource</i><i
-                    className="fa fa-caret-down" aria-hidden="true"/></div>
+                <div className={styles.dropdown} onClick={this.toggleDropdown.bind(this)}>
+                    <i>Categories</i> {dropDownIcon}
+                </div>
                 {this.state.opened ?
-                    <div className={dropDownClass}>
-                        <NavLink exact activeClassName={styles.active} to='/'>Home</NavLink>
-                        <NavLink activeClassName={styles.active} to='/category/entertainment'>Entertainment</NavLink>
-                        <NavLink activeClassName={styles.active} to='/category/gaming'>Gaming</NavLink>
-                        <NavLink activeClassName={styles.active} to='/category/general'>General</NavLink>
-                        <NavLink activeClassName={styles.active} to='/category/music'>Music</NavLink>
-                        <NavLink activeClassName={styles.active} to='/category/politics'>Politics</NavLink>
-                        <NavLink activeClassName={styles.active} to='/category/science-and-nature'>Science and
-                            Nature</NavLink>
-                        <NavLink activeClassName={styles.active} to='/category/sport'>Sport</NavLink>
-                        <NavLink activeClassName={styles.active} to='/category/technology'>Technology</NavLink>
-                    </div>
+                    <ul className={dropDownClass} onClick={this.toggleDropdown.bind(this)}>
+                        <li><NavLink exact activeClassName={styles.active} to='/'>Home</NavLink></li>
+                        <li><NavLink activeClassName={styles.active} to='/category/entertainment'>Entertainment</NavLink></li>
+                        <li><NavLink activeClassName={styles.active} to='/category/gaming'>Gaming</NavLink></li>
+                        <li><NavLink activeClassName={styles.active} to='/category/general'>General</NavLink></li>
+                        <li><NavLink activeClassName={styles.active} to='/category/music'>Music</NavLink></li>
+                        <li><NavLink activeClassName={styles.active} to='/category/politics'>Politics</NavLink></li>
+                        <li><NavLink activeClassName={styles.active} to='/category/science-and-nature'>Science and
+                            Nature</NavLink></li>
+                        <li><NavLink activeClassName={styles.active} to='/category/sport'>Sport</NavLink></li>
+                        <li><NavLink activeClassName={styles.active} to='/category/technology'>Technology</NavLink></li>
+                    </ul>
                     : ''
                 }
 
